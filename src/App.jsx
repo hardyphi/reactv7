62880b016096fda868574b7d234fddaa0e6c602d
Phil Hardy <phardy@scottlogic.com>
2022-09-08 22:51:03 +0100

    search component and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./Pet.jsx";
+import SearchParams from "./SearchParams.jsx";
 
 const App = () => {
   return (
     <div id="my-app">
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="pig" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
